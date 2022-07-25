86a5f454bfe69a24348fb48210f11d9f57317c17
lachlanvass <lachlan.vass.dev@gmail.com>
2022-07-25 13:45:14 +1000

    test: add decrement tests

--- app/src/androidTest/java/com/lachlanvass/tapcounter/presentation/components/counter/CounterTest.kt
+++ app/src/androidTest/java/com/lachlanvass/tapcounter/presentation/components/counter/CounterTest.kt
@@ -55,4 +55,52 @@ class CounterTest {
             .onNode(hasTestTag(CounterTestTag.CountValue.tag))
             .assert(hasText("100"))
     }
+
+    @Test
+    fun testDecrementFrom10() {
+
+        composeTestRule.setContent {
+            Counter()
+        }
+
+        val plusButton = composeTestRule
+            .onNode(
+                hasTestTag(
+                    CounterTestTag
+                        .PlusButton
+                        .tag
+                )
+            )
+
+        repeat(10) {
+            plusButton.performClick()
+        }
+
+        composeTestRule
+            .onNode(hasTestTag(CounterTestTag.SubtractButton.tag))
+            .performClick()
+
+        composeTestRule
+            .onNode(hasTestTag(CounterTestTag.CountValue.tag))
+            .assert(hasText("9"))
+    }
+
+    @Test
+    fun testDecrementIsNotNegative() {
+
+        composeTestRule.setContent {
+            Counter()
+        }
+
+        repeat(10) {
+
+            composeTestRule
+                .onNode(hasTestTag(CounterTestTag.SubtractButton.tag))
+                .performClick()
+        }
+
+        composeTestRule
+            .onNode(hasTestTag(CounterTestTag.CountValue.tag))
+            .assert(hasText("0"))
+    }
 }
